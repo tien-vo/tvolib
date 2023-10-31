70ac4889b8517f64aa3d4be62fcb7031c80a286c
Tien Vo <tien.a.vo@proton.me>
2023-10-31 10:41:04 -0600

    Forgot to update imports

--- src/tvolib/mpl_utils/__init__.py
+++ src/tvolib/mpl_utils/__init__.py
@@ -1,7 +1,7 @@
 import matplotlib as mpl
 
 from ._draw import draw_arrows, draw_earth, draw_multicolored_line
-from ._format import add_colorbar, format_datetime_axis
+from ._format import add_colorbar, add_text, format_datetime_axis
 from ._setup import setup
 
 mplc = mpl.colors
--- src/tvolib/mpl_utils/_format.py
+++ src/tvolib/mpl_utils/_format.py
@@ -1,5 +1,6 @@
 __all__ = [
     "add_colorbar",
+    "add_text",
     "format_datetime_axis",
 ]
 
@@ -13,13 +14,13 @@ def add_colorbar(axis, where="right", pad=0.05, size="2%"):
     return divider.append_axes(where, size=size, pad=pad)
 
 
-def format_datetime_axis(axis):
-    locator = mdates.AutoDateLocator()
-    formatter = mdates.ConciseDateFormatter(locator)
-    axis.xaxis.set_major_formatter(formatter)
-
-
 def add_text(axis, x, y, text, **kwargs):
     transform = axis.transAxes
     box_format = dict(facecolor="wheat", alpha=0.9)
     axis.text(x, y, text, transform=transform, bbox=box_format, **kwargs)
+
+
+def format_datetime_axis(axis):
+    locator = mdates.AutoDateLocator()
+    formatter = mdates.ConciseDateFormatter(locator)
+    axis.xaxis.set_major_formatter(formatter)
