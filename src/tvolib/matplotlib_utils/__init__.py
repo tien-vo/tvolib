a375ea3487c0a83fa719e29a9ac95c74571b44cb
Tien Vo <tvo.work@proton.me>
2024-05-29 14:43:45 -0600

    matplotlib_utils: Add documentation to setup routine

--- src/tvolib/matplotlib_utils/__init__.py
+++ src/tvolib/matplotlib_utils/__init__.py
@@ -2,8 +2,10 @@ import matplotlib as mpl
 
 from ._draw import draw_arrows, draw_earth, draw_multicolored_line
 from ._format import add_colorbar, add_text, format_datetime_axis
-from ._setup import setup
+from ._setup import _setup_matplotlib
 
+_setup_matplotlib()
+
+# Aliases
 mplc = mpl.colors
 plt = mpl.pyplot
-setup()
--- src/tvolib/matplotlib_utils/_setup.py
+++ src/tvolib/matplotlib_utils/_setup.py
@@ -1,4 +1,4 @@
-__all__ = ["setup"]
+__all__ = ["_setup_matplotlib"]
 
 import atexit
 import os
@@ -9,15 +9,27 @@ from importlib.resources import files
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+_latex_binary = shutil.which("latex")
+_stylesheet = files("tvolib.matplotlib_utils") / "data" / "tvolib.mplstyle"
 
-def _checkdep_usetex(s):
-    return s and shutil.which("tex")
 
+def _setup_matplotlib(use_tex=False, cache_tex=False):
+    r"""
+    Configure matplotlib with custom stylesheet and settings
 
-def setup(tex=False, cache=False, **kw):
-    plt.style.use(files("tvolib.mpl_utils") / "data" / "mpl_utils.mplstyle")
-    plt.rc("text", usetex=_checkdep_usetex(tex))
-    if cache:
+    Parameter
+    ---------
+    use_tex: bool
+        Enable latex if toggled and a binary is found in the system path
+    cache_tex: bool
+        Configure caching behavior of matplotlib for latex outputs.
+        Recommended for multiprocessing
+    """
+
+    plt.style.use(_stylesheet)
+    plt.rc("text", usetex=use_tex and _latex_binary is not None)
+
+    if cache_tex:
         # Quick fix for matplotlib's tex cache in multiprocessing
         mpldir = tempfile.mkdtemp()
         atexit.register(shutil.rmtree, mpldir)
