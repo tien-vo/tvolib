5ef9b1018bc0f168c69cfeb553050b538f5fa230
Tien Vo <tvo.work@proton.me>
2024-05-30 14:31:51 -0600

    matplotlib_utils: Add documentation and type hinting for format routines, add_text renamed to add_panel_label, add initial notebook for matplotlib_utils

--- src/tvolib/matplotlib_utils/__init__.py
+++ src/tvolib/matplotlib_utils/__init__.py
@@ -2,7 +2,7 @@ import colorcet
 import matplotlib as mpl
 
 from ._draw import draw_arrows, draw_earth, draw_multicolored_line
-from ._format import add_colorbar, add_text, format_datetime_axis
+from ._format import add_colorbar, add_panel_label, format_datetime_labels
 from ._setup import _setup_matplotlib
 
 _setup_matplotlib()
--- src/tvolib/matplotlib_utils/_format.py
+++ src/tvolib/matplotlib_utils/_format.py
@@ -1,26 +1,82 @@
 __all__ = [
     "add_colorbar",
-    "add_text",
-    "format_datetime_axis",
+    "add_panel_label",
+    "format_datetime_labels",
 ]
 
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-def add_colorbar(axis, where="right", pad=0.05, size="2%"):
-    r"""Adds colorbar next to an axis"""
-    divider = make_axes_locatable(axis)
+def add_colorbar(
+    ax: plt.Axes,
+    where: str = "right",
+    pad: float = 0.05,
+    size: str = "2%",
+) -> plt.Axes:
+    r"""
+    Adds colorbar next to a given `Axes` instance. See `matplotlib`
+    documentation for `where`, `pad`, and `size` options.
+
+    Parameters
+    ----------
+    ax: `matplotlib.axes.Axes`
+        The `Axes` instance to draw on.
+    where: str
+        Where to append the colorbar with respective to the `Axes`.
+    pad: str
+        Padding of the colorbar from the `Axes`.
+    size: str
+        Size of the colorbar relative to the `Axes`.
+
+    Return
+    ------
+    cax: `matplotlib.axes.Axes`
+        A child `Axes` instance created relative to the input `Axes` for
+        drawing the colorbar.
+    """
+    divider = make_axes_locatable(ax)
     return divider.append_axes(where, size=size, pad=pad)
 
 
-def add_text(axis, x, y, text, **kwargs):
-    transform = axis.transAxes
-    box_format = dict(facecolor="wheat", alpha=0.9)
-    axis.text(x, y, text, transform=transform, bbox=box_format, **kwargs)
+def add_panel_label(
+    ax: plt.Axes,
+    x: float,
+    y: float,
+    text: str,
+    box_format: dict = dict(facecolor="wheat", alpha=0.9),
+    **kwargs,
+) -> None:
+    r"""
+    Adds a boxed text to an `Axes` instance. Intended for drawing panel
+    annotations for a figure.
+
+    Parameters
+    ----------
+    ax: `matplotlib.axes.Axes`
+        The `Axes` instance to draw on.
+    x, y: float
+        Location of the text.
+    text: str
+        Content of the the text.
+    box_format: dict
+        Formatting of the enclosing box.
+    kwargs: dict
+        Other options for `Axes.text` method.
+    """
+    ax.text(x, y, text, transform=ax.transAxes, bbox=box_format, **kwargs)
 
 
-def format_datetime_axis(axis):
+def format_datetime_labels(ax: plt.Axes):
+    r"""
+    Format the labels for plots with `numpy.datetime64` abscissa.
+
+    Parameter
+    ---------
+    ax: `matplotlib.axes.Axes`
+        The `Axes` instance.
+    """
     locator = mdates.AutoDateLocator()
     formatter = mdates.ConciseDateFormatter(locator)
-    axis.xaxis.set_major_formatter(formatter)
+    ax.xaxis.set_major_formatter(formatter)
